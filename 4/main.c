daa22700eeaa5d9bb918d5a7788b3c0e6ba32b05
Hero <nick19960723@gmail.com>
2023-03-28 02:53:31 +0800

    finish kill and revolution

--- 4/main.c
+++ 4/main.c
@@ -7,17 +7,16 @@ typedef struct Player
 {
     int index;
     int attack_power;
-    int score;
+    // int score;
 } Player;
 
 typedef struct DSA
 {
-    uint16_t size;
-    uint16_t head;
-    uint16_t tail;
-
     // An array of Players
     Player *area;
+    uint16_t size;
+    int16_t head;
+    int16_t tail;
 } DSA;
 
 DSA *make_DSA(int16_t size)
@@ -28,12 +27,23 @@ DSA *make_DSA(int16_t size)
     return dsa;
 }
 
-int kill_othoer_players(DSA *dsa, int attack_power)
+int16_t kill_othoer_players(DSA *dsa, int attack_power)
 {
+    int16_t size = dsa->size;
+    int16_t new_tail = (dsa->tail - 1 + size) % size, count = 1;
+    while (new_tail != dsa->head)
+    {
+        new_tail = (new_tail - 1 + size) % size;
+        if (dsa->area[new_tail].attack_power >= attack_power)
+            break;
+        count++;
+    }
+    return count;
 }
 
 void revolution(DSA *dsa)
 {
+    dsa->head = (dsa->head + 1) % dsa->size;
 }
 
 void add_player(DSA *dsa, int index, int attack_power)
@@ -41,7 +51,7 @@ void add_player(DSA *dsa, int index, int attack_power)
     Player *target = &(dsa->area[dsa->tail]);
     target->index = index;
     target->attack_power = attack_power;
-    target->score = 0;
+    // target->score = 0;
     dsa->tail = (dsa->tail + 1) % dsa->size;
 }
 
@@ -50,21 +60,23 @@ void *enter_DSA(DSA *dsa, int index, int attack_power)
     Player *area = dsa->area;
     uint16_t size = dsa->size, head = dsa->head, tail = dsa->tail;
     int kill_count = 0;
-
-    if (area[tail - 1].attack_power < attack_power)
+    printf("Round %d:", index);
+    if (area[(tail - 1 + size) % size].attack_power < attack_power && area[(tail - 1 + size) % size].index != 0)
     {
         kill_count = kill_othoer_players(dsa, attack_power);
         for (int i = 1; i <= kill_count; i++)
         {
-            printf(" %d,%d", area[(tail - i + size) % size].index, area[(tail - i + size) % size].score);
+            printf(" %d", area[(tail - i + size) % size].index);
         }
+        tail = (tail - kill_count + size) % size;
+        dsa->tail = tail;
     }
     else
     {
         if (tail == head && area[head].index != 0)
         {
             revolution(dsa);
-            printf(" %d,%d", area[tail].index, area[tail].score);
+            printf(" %d", area[tail].index);
         }
     }
     add_player(dsa, index, attack_power);
@@ -73,14 +85,20 @@ void *enter_DSA(DSA *dsa, int index, int attack_power)
 
 void *final_DSA(DSA *dsa)
 {
+    int16_t head = dsa->head, target = dsa->tail, size = dsa->size;
     printf("Final:");
+    while (target != head)
+    {
+        target = (target - 1 + size) % size;
+        printf(" %d", dsa->area[target].index);
+    }
     printf("\n");
 }
 
 int main()
 {
     int n, m, input;
-    scanf("%d %d\n", &n, &m);
+    scanf("%d %d", &n, &m);
 
     // Make a "Digital Savage Arena" (DSA)
     DSA *dsa = make_DSA(m);
