1fd73b8d83135d61b4a11c9c99a6ab48b2b292e2
許興宇 <hero@xuxingyudeMacBook-Pro.local>
2023-04-23 21:11:24 +0800

    version 3

--- 7/main.c
+++ 7/main.c
@@ -4,23 +4,23 @@ typedef long long int ll_int;
 
 typedef struct SalesEvent {
     ll_int price;
-    int expired_day;
+    ll_int expired_day;
 } SalesEvent;
 
 typedef struct PriceSchedule {
-    int length;
-    int tail;
+    ll_int length;
+    ll_int tail;
     SalesEvent* sales_event_heap;
 } PriceSchedule;
 
 typedef struct Company {
     ll_int total_price;
-    int total_number_of_melon;
-    int upstream;
+    ll_int total_number_of_melon;
+    ll_int upstream;
     PriceSchedule* price_schedule;
 } Company;
 
-PriceSchedule* makePriceSchedule(int length)
+PriceSchedule* makePriceSchedule(ll_int length)
 {
     PriceSchedule* price_schedule = (PriceSchedule*)calloc(1, sizeof(PriceSchedule));
     SalesEvent* sales_event_heap = (SalesEvent*)calloc(length, sizeof(SalesEvent));
@@ -31,7 +31,7 @@ PriceSchedule* makePriceSchedule(int length)
 
 void swapSalesEvent(SalesEvent* target1, SalesEvent* target2)
 {
-    int tmp_expired_day = target2->expired_day;
+    ll_int tmp_expired_day = target2->expired_day;
     ll_int tmp_price = target2->price;
     target2->expired_day = target1->expired_day;
     target2->price = target1->price;
@@ -39,34 +39,37 @@ void swapSalesEvent(SalesEvent* target1, SalesEvent* target2)
     target1->price = tmp_price;
 }
 
-void insert_PS(PriceSchedule* ps, ll_int price, int expired_day, int day)
+void insert_PS(PriceSchedule* ps, ll_int price, ll_int expired_day, ll_int day)
 {
     if (ps->tail == ps->length)
         return;
 
-    int tmp = ps->tail;
+    ll_int tmp = ps->tail;
     ps->tail += 1;
-    SalesEvent* target = &(ps->sales_event_heap[tmp]);
-    target->price = price;
-    target->expired_day = expired_day;
+    SalesEvent *target, *parent;
     while (tmp > 0) {
-        tmp = (tmp - 1) / 2;
-        SalesEvent* parent = &(ps->sales_event_heap[tmp]);
+        target = &(ps->sales_event_heap[tmp]);
+        parent = &(ps->sales_event_heap[(tmp - 1) / 2]);
         if (parent->expired_day >= day && parent->price <= target->price)
             break;
         swapSalesEvent(target, parent);
         target = parent;
+        tmp = (tmp - 1) / 2;
     }
+    target->expired_day = expired_day;
+    target->price = price;
 }
 
 void pop_PS(PriceSchedule* ps)
 {
     if (ps->tail == 0)
         return;
-    int root = 0, tmp = ps->tail - 1, left = root * 2 + 1, right = root * 2 + 2, min;
+    ll_int root = 0, tmp = ps->tail - 1, left, right, min;
     SalesEvent* heap = ps->sales_event_heap;
     ps->tail = ps->tail - 1;
     while (root < tmp) {
+        left = root * 2 + 1;
+        right = root * 2 + 2;
         min = tmp;
         if (left < tmp) {
             if (heap[left].price < heap[min].price)
@@ -76,34 +79,29 @@ void pop_PS(PriceSchedule* ps)
             if (heap[right].price < heap[min].price)
                 min = right;
         }
-        if (min != tmp) {
-            swapSalesEvent(&heap[min], &heap[root]);
+        swapSalesEvent(&heap[min], &heap[root]);
+        if (min != tmp)
             root = min;
-            left = root * 2 + 1;
-            right = root * 2 + 2;
-        }
-        if (min == tmp) {
-            swapSalesEvent(&heap[min], &heap[root]);
+        else
             break;
-        }
     }
 }
 
-int getPrice(PriceSchedule* ps, int day)
+ll_int getPrice(PriceSchedule* ps, ll_int day)
 {
-    while (ps->sales_event_heap[0].expired_day < day) {
+    while (ps->sales_event_heap->expired_day < day) {
         pop_PS(ps);
     }
     return ps->sales_event_heap->price;
 }
 
-Company* initCompanyArray(int N, int M)
+Company* initCompanyArray(ll_int N, ll_int M)
 {
     Company *company_arr = (Company*)calloc(N, sizeof(Company)), *target, *upstream;
+    ll_int tmp;
     for (int n = 1; n < N; n++) {
-        int tmp;
         target = &company_arr[n];
-        scanf("%d", &tmp);
+        scanf("%lld", &tmp);
         target->total_number_of_melon = 1;
         target->upstream = tmp - 1;
         target->price_schedule = makePriceSchedule(M);
@@ -119,40 +117,37 @@ Company* initCompanyArray(int N, int M)
     return company_arr;
 }
 
-void purchase(int N, ll_int C, Company* company_arr, int day)
+void purchase(ll_int N, ll_int C, Company* company_arr, ll_int day)
 {
     ll_int price;
-    int total_number_of_melons = 0;
+    ll_int total_number_of_melon = 0;
     Company *target, *upstream;
     for (int n = N - 1; n >= 0; n--) {
         company_arr[n].total_price = 0;
     }
-    for (int n = N - 1; n > 0; n--) {
+    for (int n = N - 1; n >= 0; n--) {
         target = &company_arr[n];
-        upstream = &company_arr[target->upstream];
         price = getPrice(target->price_schedule, day);
         target->total_price += price;
+        if (target->total_price <= C && target->total_number_of_melon > total_number_of_melon)
+            total_number_of_melon = target->total_number_of_melon;
+        if (n == 0)
+            break;
+        upstream = &company_arr[target->upstream];
         upstream->total_price += target->total_price;
-        if (target->total_price <= C && target->total_number_of_melon > total_number_of_melons)
-            total_number_of_melons = target->total_number_of_melon;
     }
-    target = &company_arr[0];
-    price = getPrice(target->price_schedule, day);
-    target->total_price += price;
-    if (target->total_price <= C && target->total_number_of_melon > total_number_of_melons)
-        total_number_of_melons = target->total_number_of_melon;
-    printf("%d\n", total_number_of_melons);
+    printf("%lld\n", total_number_of_melon);
 }
 
-void startPurchasePlan(int N, int M, ll_int C, Company* company_arr)
+void startPurchasePlan(ll_int N, ll_int M, ll_int C, Company* company_arr)
 {
-    int expired_day;
+    ll_int expired_day;
     ll_int price;
     for (int m = 0; m < M; m++) {
 
         // Update price schedule
         for (int n = 0; n < N; n++) {
-            scanf("%lld %d", &price, &expired_day);
+            scanf("%lld %lld", &price, &expired_day);
             expired_day += m;
             insert_PS(company_arr[n].price_schedule, price, expired_day, m);
         }
@@ -164,9 +159,9 @@ void startPurchasePlan(int N, int M, ll_int C, Company* company_arr)
 
 int main()
 {
-    int N, M;
+    ll_int N, M;
     ll_int C;
-    scanf("%d %d %lld", &N, &M, &C);
+    scanf("%lld %lld %lld", &N, &M, &C);
     Company* company_arr = initCompanyArray(N, M);
     startPurchasePlan(N, M, C, company_arr);
 }
